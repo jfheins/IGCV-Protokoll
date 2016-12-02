21ea0aade1e30798bf71351cf36d66eb0579ebde
Julius F. Heins <jfheins@gmail.com>
2016-12-02 11:49:46 +0100

    Migration zur Namespace-Umbenennung im Datenmodell erzeugt

---
+++ IGCV-Protokoll/Migrations/201612021045568_Renaming.cs
@@ -0,0 +1,16 @@
+namespace IGCV_Protokoll.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Renaming : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
